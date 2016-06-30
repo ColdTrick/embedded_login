7c467aed6c5dc538a2f4fb6bc114cd357fed077b
Steve Clay <steve@mrclay.org>
2016-06-30 10:11:29 -0400

    Cleanup for 1.12

--- views/default/plugins/embedded_login/settings.php
+++ views/default/plugins/embedded_login/settings.php
@@ -1,32 +1,21 @@
 <?php
 
-	$js_url = elgg_get_site_url() . "embedded_login";
+$embed = elgg_view('embedded_login/embed_html');
 
-	$site_link = elgg_view("output/url", array("text" => elgg_get_site_entity()->name, "href" => elgg_get_site_url()));
-
-	$script_code = <<<SCRIPT
-<div id="elgg-embedded-login">$site_link</div>
-<script>
-	(function(d, s, id) {
-		var js, fjs = d.getElementsByTagName(s)[0];
-  		if (d.getElementById(id)) return;
-  		js = d.createElement(s); js.id = id;
-  		js.src = "$js_url";
-  		fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'elgg-embedded-login-js'));
-</script>
-SCRIPT;
-
-	echo elgg_echo("embedded_login:settings:info");
-	echo elgg_view("input/plaintext", array("value" => $script_code, "rows" => 20, "id" => "embedded-login-textarea"));
+echo elgg_echo("embedded_login:settings:info");
+echo elgg_view("input/plaintext", [
+	"value" => $embed,
+	"rows" => 20,
+	"id" => "embedded-login-textarea",
+]);
 
 ?>
-<style type="text/css">
-	#embedded_login-settings .elgg-button-submit {
-		display: none;
-	}
+<style>
+#embedded_login-settings .elgg-button-submit {
+	display: none;
+}
 
-	#embedded-login-textarea {
-		height: 170px;
-	}
+#embedded-login-textarea {
+	height: 170px;
+}
 </style>
